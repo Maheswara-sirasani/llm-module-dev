1d1b53e34ae77da423dbc632543021f320655306
maheswara <maheswara.sirasani@thezennialpro.com>
2025-06-25 09:44:10 +0530

    adding basic python files

---
+++ basics.py
@@ -0,0 +1,19 @@
+#printing a variable
+x=42
+print(x) 
+
+
+#variable data type
+name="mahesh"
+sal=456789
+pf=1800.05
+print(type(name))
+print(type(sal))
+print(type(pf))
+
+
+
+a=[1,2,3]
+b=a
+b.append(5)
+print

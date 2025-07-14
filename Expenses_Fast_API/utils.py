ae00359518e0345ece8dd819705cea470d41e2c5
maheswara <maheswara.sirasani@thezennialpro.com>
2025-07-14 10:32:49 +0530

    added fast api

--- Expenses_Fast_API/utils.py
+++ Expenses_Fast_API/utils.py
@@ -10,12 +10,17 @@ def load_expenses() -> List[Expense]:
     if not os.path.exists(EXPENSES_FILE):
         with open(EXPENSES_FILE, "w") as f:
             json.dump([], f)
- 
     with open(EXPENSES_FILE, "r") as f:
         return [Expense(**exp) for exp in json.load(f)]
  
-# Save expenses to the JSON file
+# Save expenses to the JSON file (convert `date` to string)
 def save_expenses(expenses: List[Expense]):
+    def serialize(exp: Expense):
+        data = exp.dict()
+        data['date'] = data['date'].isoformat()  # Convert datetime.date → str
+        return data
+ 
     with open(EXPENSES_FILE, "w") as f:
-        json.dump([exp.dict() for exp in expenses], f, indent=2)
+        json.dump([serialize(exp) for exp in expenses], f, indent=2)
+ 
  
